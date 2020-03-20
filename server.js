b5954a63847194e09141e3ff7c73f427a5aa648c
Tony Fung <tfung5@gmail.com>
2020-03-19 22:09:53 -0400

    Moved onError function to separate file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = require("./app");
 const http = require("http");
 const debug = require("debug")("tutorial:server");
 const normalizePort = require("./utils/normalizePort");
+const onError = require("./utils/onError");
 
 /**
  * Get port from environment and store in Express.
@@ -169,34 +170,8 @@ server.listen(port, () => {
   console.log("Server running on port:" + port);
   createBoardFromWordList();
 });
-server.on("Error", err => onError(err));
+server.on("Error", err => onError(err, port));
 server.on("Listening", onListening);
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
 
 /**
  * Event listener for HTTP server "listening" event.
