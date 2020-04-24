03ac90ea2385bd34237225fd274a5dc93197b34c
Tony Fung <tfung5@gmail.com>
2020-04-24 10:27:29 -0400

    WIP: Moving lobby functionality from Game to Lobby

--- classes/Lobby.js
+++ classes/Lobby.js
@@ -2,7 +2,17 @@ class Lobby {
   constructor(id, name = "Lobby #" + id) {
     this.id = id;
     this.name = name;
+    this.currentPlayers = 0;
+    this.maxPlayers = 8;
+    this.resetLobby();
+    this.isGameInProgress = false;
   }
+
+  resetLobby = () => {
+    this.redTeam = new Array(4).fill(null);
+    this.blueTeam = new Array(4).fill(null);
+    this.players = {};
+  };
 
   getId = () => {
     try {
@@ -24,11 +34,56 @@ class Lobby {
     }
   };
 
+  setCurrentPlayers = (currentPlayers) => {
+    this.currentPlayers = currentPlayers;
+  };
+
+  getCurrentPlayers = () => {
+    try {
+      return this.currentPlayers;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  setMaxPlayers = (maxPlayers) => {
+    this.maxPlayers = maxPlayers;
+  };
+
+  getMaxPlayers = () => {
+    try {
+      return this.maxPlayers;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  setIsGameInProgress = (value) => {
+    this.isGameInProgress = value;
+  };
+
+  getRedTeam = () => {
+    return this.redTeam;
+  };
+
+  getBlueTeam = () => {
+    return this.blueTeam;
+  };
+
+  getIsGameInProgress = () => {
+    return this.isGameInProgress;
+  };
+
   getLobby = () => {
     try {
       return {
         id: this.getId(),
         name: this.getName(),
+        currentPlayers: this.getCurrentPlayers(),
+        maxPlayers: this.getMaxPlayers(),
+        redTeam: this.getRedTeam(),
+        blueTeam: this.getBlueTeam(),
+        isGameInProgress: this.getIsGameInProgress(),
       };
     } catch (err) {
       console.log(err);
